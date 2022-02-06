edc50afb8ba8de339ecc5a2efeef6948b5a559a3
Cpreet <cpreet.chawla@gmail.com>
2022-02-06 23:23:49 +0530

    question 1 done

--- src/com/datastructureassiginmentsolution/classes/Building.java
+++ src/com/datastructureassiginmentsolution/classes/Building.java
@@ -1,23 +1,52 @@
 package com.datastructureassiginmentsolution.classes;
 
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EmptyStackException;
 
 public class Building {
-    public int[] buildingInput() {
+
+    Stack<Integer> buildStack = new Stack<>();
+    ArrayList<Integer> floorList = new ArrayList<>();
+
+    public void buildingInput() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the total number of floors in the building");
         
         int buildLength = input.nextInt();
-        int[] buildList = new int[buildLength];
         
         for (int i = 0; i < buildLength; i++ ) {
             System.out.println("Enter the size of the floor delivered on day: " + (i + 1));
-            buildList[i] = input.nextInt();
+            floorList.add(input.nextInt());
         }
         input.close();
-
-        return buildList;
     }
 
-    
+    public void buildOrder() {
+        //find max floor size
+        Integer max = Collections.max(floorList);
+        for (int i = 0; i < floorList.size(); i++) {
+            System.out.println("Floors built on Day " + (i+1) +": ");
+            if (floorList.get(i) >= max) {
+                System.out.print(floorList.get(i) + " ");
+                floorList.remove(i);
+                max = Collections.max(floorList);
+                try 
+                {
+                    while (buildStack.peek() >= max) {
+                        System.out.print(buildStack.pop() + " ");
+                    }
+                }
+                catch (EmptyStackException e) {
+                    System.out.print(" ");
+                }
+            }
+            else {
+                buildStack.push(floorList.get(i));
+                floorList.remove(i);
+            }
+        }
+    }
 }
--- src/com/datastructureassiginmentsolution/driver/Main.java
+++ src/com/datastructureassiginmentsolution/driver/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Building build = new Building();
         build.buildingInput();
+        build.buildOrder();
     }        
 }
